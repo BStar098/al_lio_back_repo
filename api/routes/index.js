22f5693ca2e4da184152905c3f59e17702425c3e
Francisco <fbidonde@hotmail.com>
2022-11-04 12:10:21 -0300

    rutas checkout y envio de mails

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const users = require("./users");
 const products = require("./products");
+const checkout = require("./checkout");
 
 router.use("/users", users);
 router.use("/products", products);
-
+router.use("/checkout", checkout);
 module.exports = router;
