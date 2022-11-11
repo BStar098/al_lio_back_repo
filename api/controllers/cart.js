5f4dd55a70f1fb20a5d6992d0ea54d8e05bb0237
Bstar098 <luceropintos@abc.gob.ar>
2022-11-10 21:17:32 -0300

    solucionado crasheo al agregar el mismo producto 2 veces al carrito, ahora sólo acualiza la cantidad y precio final de la tabla ya existente

--- api/controllers/cart.js
+++ api/controllers/cart.js
@@ -2,7 +2,19 @@ const { Cart } = require("../models/index");
 const { getAll } = require("../services/cart-services");
 
 const addProductToCart = (req, res) => {
-  Cart.create(req.body).then((result) => res.send(result)); //pasar en el body productId, userId, quantity
+  const { productId, userId, quantity } = req.body;
+  Cart.findOne({ where: { productId, userId } }).then((cart) => {
+    //SI YA EXISTE ESE CARRITO EN NUESTRA BASE, UPDATEAMOS SU CANTIDAD EN VEZ DE CREARLO OTRA VEZ.
+    if (cart) {
+      const newQuantity = cart.quantity + quantity;
+      cart
+        .updateFinalPrice(productId, userId, newQuantity) //ACÁ SE GENERA EL UPDATE
+        .then((updatedCart) => {
+          res.send(updatedCart);
+        });
+    } else Cart.create(req.body).then((result) => res.send(result));
+  });
+  //pasar en el body productId, userId, quantity
 };
 
 const removeProductFromCart = (req, res) => {
